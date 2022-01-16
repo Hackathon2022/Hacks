5362d34b994300dae162f9652616f5f9791ef7c2
yuangao <yuangao_ivan@outlook.com>
2022-01-15 17:28:06 -0800

    press button line gen

--- script.js
+++ script.js
@@ -1,24 +1,56 @@
+//init
+createPressButtonGroup();
+
+//=================================================================================
+
+/**
+  * Create press buttons.
+  * @param {string} item - the name of the button
+  **/
+function createPressButtonGroup() {
+  //meat
+  createPressButtons(0, ["beef", "lamb", "pork"]);
+  linebreak = document.createElement("br");
+  document.body.appendChild(linebreak);
+
+  //veg
+  createPressButtons(1, ["lettuce", "gaiLan", "brocoli"]);
+  linebreak = document.createElement("br");
+  document.body.appendChild(linebreak);
+
+  //season
+  createPressButtons(2, ["salt", "pepper", "soySauce"]);
+  linebreak = document.createElement("br");
+  document.body.appendChild(linebreak);
+}
+
+/**
+  * Create press buttons.
+  * @param {string} item - the name of the button
+  **/
+function createPressButtons(id, items) {
+  let dev = document.createElement("dev");
+  dev.id = id;
+  items.forEach(createPressButton, dev);
+  document.body.appendChild(dev);
+}
+
 /**
   * Create a press button.
   * @param {string} item - the name of the button
-  */
+  **/
 function createPressButton(item) {
-  var bt = document.createElement('button');
+  let bt = document.createElement("button");
   bt.id = item;
+  bt.type = "button";
   bt.innerHTML = item;
+  bt.className = "pressbt";
   bt.onclick = function (event) {
     document.getElementById(event.target.id).innerHTML="hi";
-    document.getElementById(event.currentTarget.id).style="background-color:pink;";
-    console.log("create button: " + event.currentTarget.id);
+    document.getElementById(event.currentTarget.id).style="background-color:red;";
+    console.log("pressed button: " + event.currentTarget.id);
   };
-  dev.appendChild(bt);
+  console.log("create button: " + bt.id);
+  console.log(this);
+  this.appendChild(bt);
 }
-
-
-//generate buttons
-const dev = document.createElement('dev');
-
-const fruits = ["apple", "orange", "cherry"];
-fruits.forEach(createPressButton);
-
-document.body.appendChild(dev);
