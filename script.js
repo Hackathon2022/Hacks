e56dec230923b5b4e44d42cb8531d7ad321de813
yuangao <yuangao_ivan@outlook.com>
2022-01-15 16:36:38 -0800

    press button generate by js

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
-const list = document.createElement('ul');
-const info = document.createElement('p');
-const html = document.querySelector('html');
-
-info.textContent = '以下是一个动态列表。点击列表外任意位置可添加新的列表项。点击已有的列表项可修改内容。';
-
-document.body.appendChild(info);
-document.body.appendChild(list);
-
-html.onclick = function() {
-  const listItem = document.createElement('li');
-  const listContent = prompt('希望列表中显示什么内容？');
-  listItem.textContent = listContent;
-  list.appendChild(listItem);
-
-  listItem.onclick = function(e) {
-    e.stopPropagation();
-    const listContent = prompt('为列表项输入新的内容');
-    this.textContent = listContent;
+/**
+  * Create a press button.
+  * @param {string} item - the name of the button
+  */
+function createPressButton(item) {
+  var bt = document.createElement('button');
+  bt.id = item;
+  bt.innerHTML = item;
+  bt.onclick = function (event) {
+    document.getElementById(event.target.id).innerHTML="hi";
+    document.getElementById(event.currentTarget.id).style="background-color:pink;";
+    console.log("create button: " + event.currentTarget.id);
   };
-};
+  dev.appendChild(bt);
+}
+
+
+//generate buttons
+const dev = document.createElement('dev');
+
+const fruits = ["apple", "orange", "cherry"];
+fruits.forEach(createPressButton);
+
+document.body.appendChild(dev);
