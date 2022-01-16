4e931d06a5b02565fd9b446aece27be4cd6b7a99
yuangao <yuangao_ivan@outlook.com>
2022-01-15 18:40:47 -0800

    Submit tested

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 //init
+var index = 0;
 createPressButtonGroup();
 
 //=================================================================================
@@ -9,48 +10,98 @@ createPressButtonGroup();
   **/
 function createPressButtonGroup() {
   //meat
-  createPressButtons(0, ["beef", "lamb", "pork"]);
+  createPressButtons(["beef", "lamb", "pork"]);
   linebreak = document.createElement("br");
   document.body.appendChild(linebreak);
 
   //veg
-  createPressButtons(1, ["lettuce", "gaiLan", "brocoli"]);
+  createPressButtons(["lettuce", "gaiLan", "brocoli"]);
   linebreak = document.createElement("br");
   document.body.appendChild(linebreak);
 
   //season
-  createPressButtons(2, ["salt", "pepper", "soySauce"]);
+  createPressButtons(["salt", "pepper", "soySauce"]);
   linebreak = document.createElement("br");
   document.body.appendChild(linebreak);
+
+  createPressButtonSubmit();
 }
 
 /**
   * Create press buttons.
   * @param {string} item - the name of the button
   **/
-function createPressButtons(id, items) {
+function createPressButtons(items) {
   let dev = document.createElement("dev");
-  dev.id = id;
-  items.forEach(createPressButton, dev);
+  dev.id = index;
+  index +=1;
+  for (let i = 0; i < items.length; i++) {
+    createPressButton(items[i], dev);
+  }
+  createPressButtonAdd(dev);
   document.body.appendChild(dev);
 }
 
 /**
   * Create a press button.
-  * @param {string} item - the name of the button
+  * @param {string} name - the name of the button
   **/
-function createPressButton(item) {
+ function createPressButton(name, _dev) {
   let bt = document.createElement("button");
-  bt.id = item;
+  bt.id = index;
+  index += 1;
   bt.type = "button";
-  bt.innerHTML = item;
+  bt.innerHTML = name;
   bt.className = "pressbt";
   bt.onclick = function (event) {
-    document.getElementById(event.target.id).innerHTML="hi";
     document.getElementById(event.currentTarget.id).style="background-color:red;";
     console.log("pressed button: " + event.currentTarget.id);
   };
-  console.log("create button: " + bt.id);
-  console.log(this);
-  this.appendChild(bt);
+  console.log("create button: " + bt.id + "at: " + _dev);
+  _dev.appendChild(bt);
+}
+
+/**
+  * Create a press button add.
+  * @param {string} name - the name of the button
+  **/
+ function createPressButtonAdd(_dev) {
+  let bt = document.createElement("button");
+  bt.id = index;
+  index += 1;
+  bt.type = "button";
+  bt.innerHTML = "+";
+  bt.className = "pressbta";
+  bt.onclick = function (event) {
+    console.log("pressed button: " + event.currentTarget.id);
+    createPressButton(window.prompt("Enter: "), _dev);
+  };
+  console.log("create button: " + bt.id + "at: " + _dev);
+  _dev.appendChild(bt);
+}
+
+/**
+  * Create a press button submit.
+  * @param {string} item - the name of the button
+  **/
+ function createPressButtonSubmit() {
+  let dev = document.createElement("dev");
+  dev.id = index;
+  index += 1;
+
+  let bt = document.createElement("button");
+  bt.id = index;
+  index += 1;
+  bt.type = "button";
+  bt.innerHTML = "submit";
+  bt.className = "pressbts";
+  bt.onclick = function (event) {
+    document.body.innerHTML = "";
+    console.log("submit");
+    document.write("submited")
+  };
+  console.log("create button: " + bt.id + "at: " + dev);
+  dev.appendChild(bt);
+
+  document.body.appendChild(dev);
 }
